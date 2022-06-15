d18269e733a6dbdbdd5428e53c9ceee10f36084f
fereszareb <fereszareb@gmail.com>
2022-06-15 22:54:11 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 function App() {
   return (
     <Router>
