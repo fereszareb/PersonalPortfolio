5ea3e71b636e8d76bd32febc838abbf2f5f3ef63
fereszareb <fereszareb@gmail.com>
2022-06-15 20:24:45 +0100

    plan of home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,24 @@
+import React from "react";
+import AboutSection from "./HomeComponents/About-Section";
+import ContactSection from "./HomeComponents/Contact-Section";
+import ExperienceSection from "./HomeComponents/Experience-Section";
+import FooterSection from "./HomeComponents/Footer-Section";
+import HeaderSection from "./HomeComponents/Header-Section";
+import RecommandationSection from "./HomeComponents/Recommandation-section";
+import SkillsSection from "./HomeComponents/Skills-Setion";
+import WorkSection from "./HomeComponents/Work-Section";
+
 const Home = () => {
   return (
     <div>
-      <p>here is page Home</p>
+      <HeaderSection />
+      <AboutSection />
+      <ExperienceSection />
+      <SkillsSection />
+      <WorkSection />
+      <ContactSection />
+      <RecommandationSection />
+      <FooterSection />
     </div>
   );
 };
